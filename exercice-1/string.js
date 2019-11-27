80a924241cb8c55d0c8ca31244268859d28ebff1
Kany <kemuel.kany@live.fr>
2019-11-27 10:34:38 +0100

    first push

---
+++ exercice-1/string.js
@@ -0,0 +1,76 @@
+ function ucfirst(str){
+     if (typeof(str) !=="string" || str.length===0)  return "";
+     return str[0].toUpperCase()+str.substring(1);
+ }
+ console.log(ucfirst("test"));
+ console.log(ucfirst("Test"));
+ console.log(ucfirst("3est"));
+ console.log(ucfirst("rest ref"));
+ console.log(ucfirst(null));
+ console.log(ucfirst({}));
+
+ function capitalize(str){
+    if (typeof(str) !=="string" || str.length===0)  return "";
+    const array = str.toLowerCase().split(" ");
+    for (let mot=0;mot<array.length;){
+        array[mot]=ucfirst(array[mot++])
+    }
+    return array.join(" ")
+ }
+
+//  console.log(capitalize("coucou malin"));
+
+ function camelCase(str){
+     str=str.replace(/[^a-z0-9\s]/gi, '')
+    if (typeof(str) !=="string" || str.length===0)  return "";
+    const array = str.toLowerCase().split(" ");
+    for (let mot=0;mot<array.length;){
+        array[mot]=ucfirst(array[mot++])
+    }
+    return array.join("")
+
+    
+ }
+//  console.log("camelCase")
+//  console.log(camelCase("SERPENTARD /*ç_è(&:!ùHARRY"));
+
+
+ function snake_case(str){
+    if (typeof(str) !=="string" || str.length===0)  return "";
+    str=str.replace(/[^a-z0-9\s]/gi, '')
+   const array = str.toLowerCase().split(" ");
+   for (let mot=0;mot<array.length;){
+       array[mot]=array[mot++]
+   }
+   return array.join("_")
+}
+
+// console.log("snake_case");
+// console.log(snake_case("SERPENTARD /*ç_è(&:!ùHARRY"));
+
+
+function leet(str){
+    if (typeof(str) !=="string" || str.length===0)  return "";
+    const dico={"a":4,
+                "e":3,
+                "i":1,
+                "o":0,
+                "u":"(_)",
+                "y":7}  
+    for (key in dico){
+        str=str.replace(new RegExp(key, 'g'), dico[key])
+    }
+    return str
+}
+
+// console.log(leet("lala"))
+
+
+// prairie={}
+// prairie["animal"]={}
+// prairie["animal"]["type"]={}
+// prairie["animal"]["type"]["name"]="Chien"
+// function prop_access(object,path){
+//     var path=path.split
+// }
+
