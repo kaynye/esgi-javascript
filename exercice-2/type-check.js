a16b4e39db0d86edc0dc9ccad55ae14f9dd92601
Kany <kemuel.kany@live.fr>
2019-11-29 14:29:35 +0100

    type

--- exercice-2/type-check.js
+++ exercice-2/type-check.js
@@ -2,7 +2,7 @@
 function type_check_v1(elem,type){
     if (elem===null && type==="object") return false
     if (elem===undefined && type==="object") return false
-    if (Array.isArray(elem)===true && type==="object") return false
+    if (elem.constructor===Array && type==="Array") return tr
     return typeof(elem)==type
 }
 
