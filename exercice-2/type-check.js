2c1ceb7c68374cac9daae68e10fb9b02860b5154
Kany <kemuel.kany@live.fr>
2019-11-29 14:09:27 +0100

    typeCheck

---
+++ exercice-2/type-check.js
@@ -0,0 +1,12 @@
+
+function type_check_v1(elem,type){
+    return typeof(elem)==type
+}
+
+console.log(type_check_v1("coucou","string"))
+
+function type_check_v2(elem,carac){
+    for ( element in carac){
+        elem
+    }
+}
